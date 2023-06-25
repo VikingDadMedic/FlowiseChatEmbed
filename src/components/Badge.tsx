aef839ec968467e1eb05ba2bcdcf04ddd1716252
Ryan E <91684138+VikingDadMedic@users.noreply.github.com>
2023-06-25 13:05:41 -0400

    updates

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,7 +51,7 @@ export const Badge = (props: Props) => {
       "text-align": 'center',
       color: props.poweredByTextColor ?? defaultTextColor,
       "background-color": props.badgeBackgroundColor ?? '#ffffff'
-    }}>Powered by
+    }}> Powered by
       <a
         ref={liteBadge}
         href={'https://www.roxie.ai'}
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span>Roxie</span>
+        <span>Roxie AI</span>
       </a>
     </span>
   )
